894c63a2112b88bc9fbe2680fcd6de7434bbf6f6
Tony Mputhia <tonymmputhia@gmail.com>
2025-02-18 12:07:03 +0300

    Modify main app component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero";
+import Questionnaire from "../components/Questionnaire";
+import ProductShowcase from "../components/ProductShowcase";
+import Footer from "../components/Footer";
+
+const Home: React.FC = () => (
+  <>
+    <Hero />
+    <Questionnaire />
+    <ProductShowcase />
+    <Footer />
+  </>
+);
+export default Home;
