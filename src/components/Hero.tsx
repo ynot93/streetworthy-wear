5ee81f8297b37d1920fdc22057c3d41a45f1221b
Tony Mputhia <tonymmputhia@gmail.com>
2025-06-06 09:54:21 +0300

    Add login, register and product links on home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,16 @@
 import heroImg from '../assets/hero-image.png';
 import logoImg from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const HeroFC = () => (
   <section className='relative min-h-screen flex flex-col md:flex-row items-center justify-between p-4 bg-black text-white'>
     <div className="md:absolute md:top-0 md:left-4">
-      <img src={logoImg} alt="Logo" className="h-28 w-auto" /> {/* Adjust h-16 for logo height */}
+      <img src={logoImg} alt="Logo" className="h-28 w-auto" />
+      <ul>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/products">Browse Products</Link></li>
+      </ul>
     </div>
     <div className='md:w-1/2 text-center'>
       <h1 className='text-6xl md:text-6xl font-bold'>We in The Streets</h1>
