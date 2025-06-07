244546822f4f20827c551d2359f2f48656dd13fc
Tony Mputhia <tonymmputhia@gmail.com>
2025-06-07 09:15:54 +0300

    Update product list and product page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import HomePage from "./pages/HomePage";
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-// import ProductListPage from './pages/ProductListPage'; // Will add later
-// import ProductDetailPage from './pages/ProductDetailPage'; // Will add later
+import ProductListPage from './pages/ProductListPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 // import CartPage from './pages/CartPage'; // Will add later
 // import CheckoutPage from './pages/CheckoutPage'; // Will add later
 // import Dashboard from './pages/Dashboard'; // Admin dashboard later
@@ -15,8 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        {/* <Route path="/products" element={<ProductListPage />} /> */}
-        {/* <Route path="/products/:id" element={<ProductDetailPage />} /> */}
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
         {/* <Route path="/cart" element={<CartPage />} /> */}
         {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
       </Routes>
