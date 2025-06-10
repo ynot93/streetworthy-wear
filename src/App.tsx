1aa26e196c5f2b34d04044d66600fb3af7ed6914
Tony Mputhia <tonymmputhia@gmail.com>
2025-06-10 13:44:04 +0300

    Add cart context globally for shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
+import { CartProvider } from './context/CartContext';
+import CartPage from './pages/CartPage';
 // import CartPage from './pages/CartPage'; // Will add later
 // import CheckoutPage from './pages/CheckoutPage'; // Will add later
 // import Dashboard from './pages/Dashboard'; // Admin dashboard later
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/products" element={<ProductListPage />} />
-        <Route path="/products/:id" element={<ProductDetailPage />} />
-        {/* <Route path="/cart" element={<CartPage />} /> */}
-        {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
-      </Routes>
-    </div>
+    <CartProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
+        </Routes>
+      </div>
+    </CartProvider>
   );
 }
 
